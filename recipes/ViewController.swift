36faaf80a6b29abfb9d6339e060d0548a4763750
Safwa Salem <safwa.salem@gmail.com>
2022-02-01 21:42:54 +0200

    init

---
+++ recipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  recipes
+//
+//  Created by safwa salem on 2/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
