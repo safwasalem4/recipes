32a645f3236882f65c88adca83828d775052a35d
Safwa Salem <safwa.salem@gmail.com>
2022-02-06 12:46:26 +0200

    recipies search & details screens UI

--- recipes/Views/TableCell.swift
+++ recipes/Views/TableCell.swift
@@ -13,4 +13,19 @@ class TableCell: UITableViewCell {
     @IBOutlet weak var recipeTitle: UILabel!
     @IBOutlet weak var recipeSource: UILabel!
     @IBOutlet weak var healthLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+            super.awakeFromNib()
+        }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+            super.setSelected(selected, animated: animated)
+        }
+
+
+        override func layoutSubviews() {
+            super.layoutSubviews()
+            self.imageView?.frame = CGRect(x: 16,y: 16,width: 98,height: 98)
+        }
 }
