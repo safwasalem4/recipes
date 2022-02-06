516404f2325ae4d87ab2e0e368dc7bf3d7d65c0f
Safwa Salem <safwa.salem@gmail.com>
2022-02-06 09:52:33 +0200

    tabel cell ui

---
+++ recipes/Views/TableCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableCell.swift
+//  recipes
+//
+//  Created by safwa salem on 2/4/22.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var recipeTitle: UILabel!
+    @IBOutlet weak var recipeSource: UILabel!
+    @IBOutlet weak var healthLabel: UILabel!
+}
